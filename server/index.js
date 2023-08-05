c8931eba274722157a70cbd0281d0b03c5ea9080
Martin B <m.bogdanov9110@gmail.com>
2023-08-05 18:15:06 +0300

    NEW TEST SERVER WITH Express.js, mongo and GraphQl

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { graphqlHTTP } = require("express-graphql");
+const port = process.env.PORT || 5000;
+const schema = require("./schema/schema");
+// Connect to database
+require("./config/db").connecter();
+
+const app = express();
+
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+);
+
+app.listen(port, console.log(`Server running on port ${port}`));
